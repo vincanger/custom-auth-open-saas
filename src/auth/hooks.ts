2cb02b79af8d03aa59b963636c970b5fed0c5f52
vincanger <70215737+vincanger@users.noreply.github.com>
2024-12-18 14:45:55 +0100

    clean up

--- src/auth/hooks.ts
+++ src/auth/hooks.ts
@@ -1,39 +1,11 @@
 import { HttpError } from 'wasp/server';
-import type { OnAfterSignupHook, OnBeforeSignupHook } from 'wasp/server/auth';
+import { PrismaClient } from '@prisma/client';
+import type { User } from 'wasp/entities';
+import type { OnAfterSignupHook, ProviderId, OAuthData } from 'wasp/server/auth';
 import type { OnBeforeOAuthRedirectHook } from 'wasp/server/auth';
 
-export const onAfterSignup: OnAfterSignupHook = async ({ providerId, user, prisma, oauth }) => {
-  // For Stripe to function correctly, we need a valid email associated with the user.
-  // Discord allows an email address to be optional. If this is the case, we delete the user
-  // from our DB and throw an error.
-  if (providerId.providerName === 'discord' && !user.email) {
-    await prisma.user.delete({
-      where: {
-        id: user.id,
-      },
-    });
-    throw new HttpError(403, 'Discord user needs a valid email to sign up');
-  }
-
-  let userTypeRecord;
-  if (oauth) {
-    userTypeRecord = await prisma.userType.findFirst({
-      where: {
-        oauthUniqueRequestId: oauth.uniqueRequestId,
-      },
-    });
-  }
-
-  if (userTypeRecord) {
-    await prisma.user.update({
-      where: { id: user.id },
-      data: { userTypeId: userTypeRecord.id },
-    });
-  }
-};
-
 export const onBeforeOAuthRedirect: OnBeforeOAuthRedirectHook = async ({ url, oauth, prisma, req }) => {
-  const userType = req.query.userType;
+  const userType = req.query.userType; // 'creator' of 'fan'
   let userTypeRecord;
   if (userType && typeof userType === 'string') {
     userTypeRecord = await prisma.userType.create({
@@ -48,16 +20,35 @@ export const onBeforeOAuthRedirect: OnBeforeOAuthRedirectHook = async ({ url, oa
   return { url };
 };
 
-// export const onBeforeSignup: OnBeforeSignupHook = async ({ req, prisma }) => {
-//   const userType = req.query.userType;
-//   console.log(req.body)
-//   console.log(req)
-//   if (userType && typeof userType === 'string') {
-//     await prisma.userType.create({
-//       data: {
-//         type: userType,
-//         email: req.body.email,
-//       },
-//     });
-//   }
-// };
+export const onAfterSignup: OnAfterSignupHook = async ({ providerId, user, prisma, oauth }) => {
+  await validateDiscordEmail(providerId, user, prisma);
+  if (oauth) {
+    await updateUserTypeFromOAuth(user, oauth, prisma);
+  }
+};
+
+const validateDiscordEmail = async (providerId: ProviderId, user: User, prisma: PrismaClient) => {
+  if (providerId.providerName === 'discord' && !user.email) {
+    await prisma.user.delete({
+      where: {
+        id: user.id,
+      },
+    });
+    throw new HttpError(403, 'Discord user needs a valid email to sign up');
+  }
+};
+
+const updateUserTypeFromOAuth = async (user: User, oauth: OAuthData, prisma: PrismaClient) => {
+  const userTypeRecord = await prisma.userType.findFirst({
+    where: {
+      oauthUniqueRequestId: oauth.uniqueRequestId,
+    },
+  });
+
+  if (userTypeRecord) {
+    await prisma.user.update({
+      where: { id: user.id },
+      data: { userTypeId: userTypeRecord.id },
+    });
+  }
+};
